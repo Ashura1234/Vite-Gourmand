030296414497bbffeee85738b39c13e40af09342
Ashura1234 <mpouhililou.emmanuel.com>
2026-03-22 16:12:29 +0100

    [ADD] Routing, bootstrap custom, header

---
+++ Router/allRoutes.js
@@ -0,0 +1,6 @@
+import Route from "./Route.js";
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/menu", "Menu", "/pages/menu.html"),
+];
+export const websiteName = "Vite & Gourmand";
