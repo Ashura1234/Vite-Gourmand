55462fb9b20f6656a6a68be25b96d7b00bcbf448
Ashura1234 <mpouhililou.emmanuel.com>
2026-04-20 16:49:39 +0200

    [EDIT & ADD] menu bordelais et bugs des boutons de la page menu fixed.

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,5 +2,6 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/menu", "Menu", "/pages/menu.html"),
+    new Route("/menuBordelais", "Menu bordelais", "/pages/menu/menuBordelais.html"),
 ];
 export const websiteName = "Vite & Gourmand";
