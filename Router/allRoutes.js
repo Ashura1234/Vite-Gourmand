4efee8dca9fbff914dc28d29a18ab6cac6612d77
Ashura1234 <mpouhililou.emmanuel.com>
2026-04-22 13:52:09 +0200

    [ADD] menu printanier, prestige, Terre et mer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,5 +3,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/menu", "Menu", "/pages/menu.html"),
     new Route("/menuBordelais", "Menu bordelais", "/pages/menu/menuBordelais.html"),
+    new Route("/menuPrintanier", "Menu Printanier", "/pages/menu/menuPrintanier.html"),
+    new Route("/menuPrestige", "Menu Prestige", "/pages/menu/menuPrestige.html"),
+    new Route("/menuTerreEtMer", "Menu Terre et Mer", "/pages/menu/menuTerreEtMER.html"),
 ];
 export const websiteName = "Vite & Gourmand";
