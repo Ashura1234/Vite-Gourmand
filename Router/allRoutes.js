75918fe2335879fbcb422db47f3b56e96a2c2f93
Ashura1234 <mpouhililou.emmanuel.com>
2026-04-24 14:56:52 +0200

    [ADD] tout les autres menus manquant

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,5 +6,13 @@ export const allRoutes = [
     new Route("/menuPrintanier", "Menu Printanier", "/pages/menu/menuPrintanier.html"),
     new Route("/menuPrestige", "Menu Prestige", "/pages/menu/menuPrestige.html"),
     new Route("/menuTerreEtMer", "Menu Terre et Mer", "/pages/menu/menuTerreEtMER.html"),
+    new Route("/menuVegetarien", "Menu végétarien", "/pages/menu/menuVegetarien.html"),
+    new Route("/menuEnfant", "Menu Enfant", "/pages/menu/menuEnfant.html"),
+    new Route("/menuCocktail", "Menu Cocktail", "/pages/menu/menuCocktail.html"),
+    new Route("/menuBrunch", "Menu Brunch", "/pages/menu/menuBrunch.html"),
+    new Route("/menuAutomne", "Menu Automne", "/pages/menu/menuAutomne.html"),
+    new Route("/menuSushi", "Menu Sushi", "/pages/menu/menuSushi.html"),
+    new Route("/menuEntreprise", "Menu Entreprise", "/pages/menu/MenuEntreprise.html"),
+    new Route("/menuPoisson", "Menu Poisson", "/pages/menu/menuPoisson.html"),
 ];
 export const websiteName = "Vite & Gourmand";
