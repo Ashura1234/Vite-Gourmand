e470fba377f292b42600d04e31ce2cf2a98babbe
Ashura1234 <mpouhililou.emmanuel@gmail.com>
2026-04-27 15:08:45 +0200

    [ADD] modals de payement

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -14,5 +14,7 @@ export const allRoutes = [
     new Route("/menuSushi", "Menu Sushi", "/pages/menu/menuSushi.html"),
     new Route("/menuEntreprise", "Menu Entreprise", "/pages/menu/MenuEntreprise.html"),
     new Route("/menuPoisson", "Menu Poisson", "/pages/menu/menuPoisson.html"),
+    new Route("/connexion", "Connexion", "/pages/auth/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/auth/inscription.html"),
 ];
 export const websiteName = "Vite & Gourmand";
